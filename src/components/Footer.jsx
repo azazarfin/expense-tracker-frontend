a40256e7591d93468eb06f56c561844587583a15
Azaz Arfin <azazarfin54@gmail.com>
2025-09-12 11:28:30 +0600

    Footer Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="w-full text-center py-6">
       <p className="text-lg font-handwritten text-gray-700 dark:text-gray-300">
-        Made by Azaz with <span className="text-red-500">❤</span>
+        Made by Azaz(ECE'23) with <span className="text-blue-500">❤</span>
       </p>
     </footer>
   );
